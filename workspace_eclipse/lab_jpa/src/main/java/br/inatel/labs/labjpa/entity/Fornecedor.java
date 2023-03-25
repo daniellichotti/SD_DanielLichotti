ab3ed145e665468ccac1902dfa355667399016fe
daniellichotti <danielclichotti@gmail.com>
2023-03-25 15:06:12 -0300

    Codigos aula 25-03

---
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Fornecedor.java
@@ -0,0 +1,9 @@
+package br.inatel.labs.labjpa.entity;
+
+import java.util.List;
+
+public class Fornecedor {
+	private Long id;
+	private List<Produto> listaProduto;
+	private String razaoSocial;
+}
---
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/NotaCompraItem.java
@@ -0,0 +1,16 @@
+package br.inatel.labs.labjpa.entity;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class NotaCompraItem {
+	private Long id;
+	private NotaCompra notaCompra;
+	private Produto produto;
+	private BigDecimal valorCompraProduto;
+	private Integer quantidade = 1;
+		
+	public BigDecimal getCalculoTotalItem() {
+		return valorCompraProduto.multiply(BigDecimal.valueOf(quantidade));
+	}
+}
