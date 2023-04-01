2831740128bc1dba6e19abc40b89fa4df1c2fa00
daniellichotti <danielclichotti@gmail.com>
2023-04-01 15:12:42 -0300

    Commitando a aula de 01-04

---
+++ workspace_eclipse/lab_jpa/src/main/java/br/inatel/labs/labjpa/service/NotaCompraService.java
@@ -0,0 +1,45 @@
+package br.inatel.labs.labjpa.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.inatel.labs.labjpa.entity.NotaCompra;
+import br.inatel.labs.labjpa.entity.NotaCompraItem;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+
+@Service
+@Transactional
+public class NotaCompraService {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	public NotaCompra salvarNotaCompra(NotaCompra nc) {
+		return em.merge(nc);
+	}
+	
+	public NotaCompra buscarNotaCompraPeloId(Long id) {
+		return em.find(NotaCompra.class, id);
+	}
+	
+	public List<NotaCompra> listarNotaCompra(){
+		return em.createQuery("select nc from NotaCompra nc", NotaCompra.class).getResultList();
+	}
+	
+	public NotaCompraItem salvarNotaCompraItem(NotaCompraItem item) {
+		return em.merge(item);
+	}
+	
+	public NotaCompraItem buscarNotaCompraItemPeloId(Long id) {
+		return em.find(NotaCompraItem.class, id);
+	}
+	
+	public List<NotaCompraItem> listarNotaCompraItem() {
+		return em.createQuery("select i from NotaCompra i", NotaCompraItem.class).getResultList();
+	}
+	
+}
