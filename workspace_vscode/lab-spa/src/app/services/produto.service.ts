7fe594c83e1f5fc4a6f4bb3770241a482f22312f
daniellichotti <danielclichotti@gmail.com>
2023-06-29 19:49:02 -0300

    enviando os codigos do lab spa

---
+++ workspace_vscode/lab-spa/src/app/services/produto.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
+import { ProdutoDTO } from '../dtos/produto.dto';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoService {
+
+  constructor(private client: HttpClient) { }
+
+  public findAll(): Observable<ProdutoDTO[]> {
+    return this.client.get<ProdutoDTO[]>('http://localhost:8081/produto');
+  }
+}
